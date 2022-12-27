f4f6dff19ed333ade7f74dd02dfd439e169dc468
Joe Malatesta <joemalatesta@msn.com>
2022-12-26 20:31:32 -0500

    restart project

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
+      <h1>This is a secret Santa site</h1>
       <h1>hello, {user ? user.name : 'friend'}</h1>
     </main>
   )
